a2fb1e827a2f59b1c641152196cd4a6610dd6a73
Linda-Andoh <lexicon736@gmail.com>
2022-08-10 11:31:17 -0400

    FizzBuz answered

--- assignment.js
+++ assignment.js
@@ -9,6 +9,19 @@
 // .......................
 // Get Even
 // .......................
-for (let i = 0; i <= 200; i +=2 ) {
-        console.log(i);
+// for (let i = 0; i <= 200; i +=2 ) {
+//         console.log(i);
+//     }
+
+// .......................
+// Fizz Buzz
+// .......................
+for (let i = 1; i <= 100; i++) {
+    if (i % 3) {
+        console.log("Fizz");
+    }else if (i % 5) {
+        console.log("Buzz");
+    }else {
+        console.log("FizzBuzz");
     }
+}
